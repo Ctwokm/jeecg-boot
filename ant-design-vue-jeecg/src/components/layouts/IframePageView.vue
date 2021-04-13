5a2cfecfd86cb85444b53bd0e1af7b0ee49373d3
HuJinwu <hjw135145@163.com>
2021-04-13 11:51:13 +0800

    开源源码有BUG，先回退

--- ant-design-vue-jeecg/src/components/layouts/IframePageView.vue
+++ ant-design-vue-jeecg/src/components/layouts/IframePageView.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <iframe  :id="id" :src="url" frameborder="0" width="100%" height="800px" scrolling="auto"></iframe>
+  <iframe  :id="id" :src="url" frameborder="0" width="100%" height="800px" scrolling="auto"></iframe>
 
 </template>
 
@@ -38,18 +38,16 @@
         //url = "http://www.baidu.com"
         console.log("------url------"+url)
         if (url !== null && url !== undefined) {
-          //-----------------------------------------------------------------------------------------
-          //url支持通过 ${token}方式传递当前登录TOKEN
-          let tokenStr = "${token}";
-          if(url.indexOf(tokenStr)!=-1) {
-            let token = Vue.ls.get(ACCESS_TOKEN);
-            this.url = url.replace(tokenStr, token);
-          }
-          //-----------------------------------------------------------------------------------------
-
+          this.url = url;
           /*update_begin author:wuxianquan date:20190908 for:判断打开方式，新窗口打开时this.$route.meta.internalOrExternal==true */
           if(this.$route.meta.internalOrExternal != undefined && this.$route.meta.internalOrExternal==true){
             this.closeCurrent();
+            //外部url加入token
+            let tokenStr = "${token}";
+            if(url.indexOf(tokenStr)!=-1){
+              let token = Vue.ls.get(ACCESS_TOKEN);
+              this.url = url.replace(tokenStr,token);
+            }
             window.open(this.url);
           }
           /*update_end author:wuxianquan date:20190908 for:判断打开方式，新窗口打开时this.$route.meta.internalOrExternal==true */
